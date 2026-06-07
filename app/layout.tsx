082d55d7e760e7f66076c399ec258394232b97c2
XDR-SAM <tdxfarhan@gmail.com>
2026-06-07 08:17:04 +0000

    Add JSON-LD ProfessionalService schema

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,13 +9,13 @@ const instrumentSans = Instrument_Sans({
   variable: '--font-instrument'
 });
 
-const instrumentSerif = Instrument_Serif({ 
+const instrumentSerif = Instrument_Serif({
   subsets: ["latin"],
   weight: "400",
   variable: '--font-instrument-serif'
 });
 
-const jetbrainsMono = JetBrains_Mono({ 
+const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
   variable: '--font-jetbrains'
 });
@@ -26,6 +26,65 @@ export const metadata: Metadata = {
   generator: 'Makezaa',
 }
 
+const jsonLd = {
+  "@context": "https://schema.org",
+  "@type": "ProfessionalService",
+  "name": "Makezaa",
+  "image": "https://makezaa.com/logo.png",
+  "@id": "https://makezaa.com",
+  "url": "https://makezaa.com",
+  "telephone": ["+8801570200990", "+8801570297033"],
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "351-3, Dhanmondi",
+    "addressLocality": "Dhaka",
+    "postalCode": "1209",
+    "addressCountry": "BD"
+  },
+  "geo": {
+    "@type": "GeoCoordinates",
+    "latitude": 23.7461,
+    "longitude": 90.3742
+  },
+  "openingHoursSpecification": {
+    "@type": "OpeningHoursSpecification",
+    "dayOfWeek": [
+      "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+    ],
+    "opens": "00:00",
+    "closes": "23:59"
+  },
+  "sameAs": [
+    "https://www.facebook.com/makezaa.inc",
+    "https://www.linkedin.com/company/makezaa"
+  ],
+  "founder": [
+    {
+      "@type": "Person",
+      "name": "Abdullah Al Sami",
+      "jobTitle": "Founder & CEO"
+    },
+    {
+      "@type": "Person",
+      "name": "Arghya Biswas",
+      "jobTitle": "Co-Founder & CTO"
+    }
+  ],
+  "employee": [
+    {
+      "@type": "Person",
+      "name": "Abdullah Al Sami",
+      "jobTitle": "CEO"
+    },
+    {
+      "@type": "Person",
+      "name": "Arghya Biswas",
+      "jobTitle": "CTO"
+    }
+  ],
+  "description": "Makezaa provides next-level web experience including Web Development, Brand Identity and SEO services."
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -33,6 +92,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+      </head>
       <body className={`${instrumentSans.variable} ${instrumentSerif.variable} ${jetbrainsMono.variable} font-sans antialiased`}>
         {children}
         <Analytics />
